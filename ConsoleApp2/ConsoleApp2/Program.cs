211df5e85af29b0ef53dce3613c7973c4d53905f
HurricaneMmax <karveshkinma@gmail.com>
2023-04-01 22:10:43 +0300

    First commit
    Получаеться дз, плюс для себя задачка

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,6 @@
+﻿using ConsoleApp2;
+
+Rectangle dota = new Rectangle(5,3);
+
+Console.WriteLine("summa" + dota.A + dota.B);
+Console.WriteLine(dota.Score());
---
+++ ConsoleApp2/ConsoleApp2/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp2;
+
+public class Rectangle
+{
+    public int A { get; set; }
+
+    public int B { get; set; }
+
+    public int Score()
+    {
+        return A * B; 
+        
+    }
+    
+    public Rectangle(int a, int b)
+    {
+        A = a;
+        B = b;
+    }
+}
